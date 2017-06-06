8d7c493dac0f7cfdc0c798d94eeedfc73e5dc6c0
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-06-07 02:20:35 +0900

    main.pyに説明追加

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-  
+
+
+# answerに詳細を書きました，動かないです…
 def main(argv, options):
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
@@ -8,17 +10,36 @@ def main(argv, options):
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
 
-    list = []
-    list.append("(")
+    node = []
+    node.append("(")
     for i, v in enumerate(argv):
       if v != " ":
-        list.append(v)
+        node.append(v)
         
-      print("argv[{0}]: {1}".format(i, v))
-    list.append(")")
-    print list
+      print("argv[{0}]: {1}".format(i, v))      
+    node.append(")")
+    print node
+
+    graph = []
+    for i in range(len(node)):
+      graph.append(node[i])
 
 
+      if graph[i] == "(":
+        list = []
+        for i in range(len(node) - len(node)):
+          
+        
+      if graph[i] == "*":
+        list = []  
+      elif graph[i] == "/":
+        list = []
+
+        
+      list.append(node[i])
+      
+
+# 木構造を多重リストで表現しようとした機構 nestが深いとうまく動かない
 def hoge(argv, options):
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
